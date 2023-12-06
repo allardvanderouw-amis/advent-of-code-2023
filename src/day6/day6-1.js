0ab50175a1cc61687449a67b546ebe9f3b1f3ec6
Allard van der Ouw <allard.van.der.ouw@amis.nl>
2023-12-06 08:21:31 +0100

    day 6

---
+++ src/day6/day6-1.js
@@ -0,0 +1,44 @@
+const input1 = `
+Time:      7  15   30
+Distance:  9  40  200
+`
+
+const input2 = `
+Time:        44     70     70     80
+Distance:   283   1134   1134   1491
+`
+
+const getRaceCombos = (times, distances, raceIndex) => {
+  const time = times[raceIndex]
+  const distance = distances[raceIndex]
+
+  let combos = 0
+  for (let ms = 0; ms < time; ms += 1) {
+    const remainingMs = time - ms
+    const comboDistance = remainingMs * ms
+    if (comboDistance > distance) {
+      combos += 1
+    }
+
+    console.log({ ms, comboDistance, combos })
+  }
+
+  console.dir({ time, distance, combos })
+  return combos
+}
+
+const run = () => {
+  let sum = 1
+  const lines = input2.trim().split('\n')
+  const times = lines[0].split(':')[1].split(' ').filter(Boolean).map(Number)
+  const distances = lines[1].split(':')[1].split(' ').filter(Boolean).map(Number)
+  console.dir({ times, distances })
+
+  for (let raceIndex = 0; raceIndex < times.length; raceIndex += 1) {
+    const combos = getRaceCombos(times, distances, raceIndex)
+    sum = sum * combos
+  }
+
+  console.log(sum)
+}
+run()
